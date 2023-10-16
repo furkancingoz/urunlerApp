4ee340cc47583779552118a991ade7b83fc31b76
Furkan Cingöz <efcingoz@outlook.com>
2023-10-16 21:49:11 +0300

    Initial Commit

---
+++ urunlerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  urunlerApp
+//
+//  Created by Furkan Cingöz on 16.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
