c1e65f0bcbaa5dd8032b4486d415644582b20f46
Furkan Cingöz <efcingoz@outlook.com>
2023-10-16 22:07:11 +0300

    tasarım oluşturma

---
+++ urunlerApp/Urunler Data.swift
@@ -0,0 +1,8 @@
+//
+//  Urunler Data.swift
+//  urunlerApp
+//
+//  Created by Furkan Cingöz on 16.10.2023.
+//
+
+import Foundation
---
+++ urunlerApp/UrunlerHucre.swift
@@ -0,0 +1,23 @@
+//
+//  UrunlerHucre.swift
+//  urunlerApp
+//
+//  Created by Furkan Cingöz on 16.10.2023.
+//
+
+import UIKit
+
+class UrunlerHucre: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
