41f9b2b391e2b65da8f611d80faa986fa88507be
Furkan Cingöz <efcingoz@outlook.com>
2023-10-17 22:20:21 +0300

    detayvcde görüntüleme

--- urunlerApp/Anasayfa.swift
+++ urunlerApp/Anasayfa.swift
@@ -19,6 +19,7 @@ class Anasayfa: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        //ürünleri tanımlama
         let u1 = Urunler(idInput: 1, adInput: "Macbook Pro 14'", resimInput: "bilgisayar", fiyatInput: 43000)
         let u2 = Urunler(idInput: 2, adInput: "Rayban Club Master", resimInput: "gozluk", fiyatInput: 2500)
         let u3 = Urunler(idInput: 3, adInput: "Sony ZX Series", resimInput: "kulaklik", fiyatInput: 4000)
@@ -26,6 +27,7 @@ class Anasayfa: UIViewController {
         let u5 = Urunler(idInput: 5, adInput: "Casio X Series", resimInput: "saat", fiyatInput: 70000)
         let u6 = Urunler(idInput: 6, adInput: "Dyson V8", resimInput: "supurge", fiyatInput: 18000)
         let u7 = Urunler(idInput: 7, adInput: "iPhone 13", resimInput: "telefon", fiyatInput: 32000)
+        //ürünlerlistesine ürünleri ekleme
         urunListesi.append(u1)
         urunListesi.append(u2)
         urunListesi.append(u3)
@@ -33,7 +35,7 @@ class Anasayfa: UIViewController {
         urunListesi.append(u5)
         urunListesi.append(u6)
         urunListesi.append(u7)
-        
+        tableView.separatorColor = UIColor(white: 0.95, alpha: 1)
     }
 
 
@@ -51,8 +53,36 @@ extension Anasayfa : UITableViewDelegate, UITableViewDataSource {
         hucre.imageViewUrun.image = UIImage(named: urun.resim!)
         hucre.urunAdLabel.text = urun.ad
         hucre.urunFiyatLabel.text = "\(urun.fiyat!) ₺"
-        
-        
+
+        hucre.backgroundColor = UIColor(white: 0.95, alpha: 1) // hücrenin arka plan rengini ayarlama
+        hucre.hucreView.layer.cornerRadius = 20
+        hucre.selectionStyle = .none //seçime tıklandığında işaretlemeyi kaldırma
         return hucre
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let urun = urunListesi[indexPath.row]
+        
+        let delAction = UIContextualAction(style: .destructive, title: "Sil") {contexualAction,view,bool in
+            print("\(urun.ad!) silindi")
+        }
+        let editAction = UIContextualAction(style: .normal, title: "Düzenle") {contexualAction,view,bool in
+            print("\(urun.ad!) düzenlendi")
+        }
+     return UISwipeActionsConfiguration(actions: [delAction,editAction])
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let urun = urunListesi[indexPath.row]
+        performSegue(withIdentifier: "toDetayVC", sender: urun) //burda urunu seçmemiz önemli sender geçiş sırasında taşınacak veriyi temsil ediyor
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetayVC" {
+            if let urun = sender as? Urunler {
+                let gidilecekVC = segue.destination as! DetaySayfa
+                gidilecekVC.urun = urun
+            }
+        }
     }
 }
--- urunlerApp/DetaySayfa.swift
+++ urunlerApp/DetaySayfa.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class DetaySayfa: UIViewController {
-
+    @IBOutlet weak var urunImageView: UIImageView!
+    @IBOutlet weak var fiyatLabel: UILabel!
+    
+    var urun:Urunler?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let u = urun {
+            self.navigationItem.title = u.ad
+            urunImageView.image = UIImage(named: u.resim!)
+            fiyatLabel.text = "\(u.fiyat!) ₺"
+        }
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sepeteEkleButton(_ sender: Any) {
+        if let u = urun {
+            print("detay sayfa \(u.ad!) sepete eklendi")
+        }
     }
-    */
+    
+  
 
 }
